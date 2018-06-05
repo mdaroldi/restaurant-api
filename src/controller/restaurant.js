ceec68043e6f00f232b1bf38c42fa9427ef5d36b
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2018-06-05 19:40:00 -0300

    Update Add new functions

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -5,7 +5,8 @@ import Restaurant from '../model/restaurant';
 export default({ config, db }) => {
     let api = Router();
 
-    // '/v1/restaurant/add'
+    // CRUD - Create Read Update Delete
+    // '/v1/restaurant/add' - Create
     api.post('/add', (req, res) => {
         let newRest = new Restaurant();
         newRest.name = req.body.name;
@@ -15,6 +16,26 @@ export default({ config, db }) => {
                 res.setEncoding(err);
             }
             res.json({ message: 'Restaurant saved successfully'});
+        });
+    });
+
+    // '/v1/restaurant' - Read
+    api.get('/', (req, res) => {
+        Restaurant.find({}, (err, restaurants) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(restaurants);
+        });
+    });
+
+    // '/v1/restaurant/:id' - Read 1
+    api.get('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(restaurant);
         });
     });
 
