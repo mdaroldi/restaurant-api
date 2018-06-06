d6b290eddeec90e93992dbbaf975cac6ff8dd665
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2018-06-06 15:50:14 -0300

    Update
    Update schema

--- src/model/restaurant.js
+++ src/model/restaurant.js
@@ -1,8 +1,22 @@
 import mongoose from 'mongoose';
+import Review from './review';
 let Schema = mongoose.Schema;
 
 let restaurantSchema = new Schema({
-    name: String
+    name: {
+        type: String,
+        required: true
+    },
+    foodtype: {
+        type: String,
+        required: true
+    },
+    avgcost: Number,
+    geometry: {
+        type: { type: String, default: 'Point'},
+        coordinates: [Number]
+    },
+    reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]
 });
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
